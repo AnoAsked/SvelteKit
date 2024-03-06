e9466dcd5fda6461fcf1af38f5fc0823b4ca78eb
Simon Masooglu <masooglu.simon@gmail.com>
2024-03-06 19:03:36 +0100

    Added iconify && Changed favicon

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,10 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
+<script>
+	import Icon from '@iconify/svelte';
+</script>
 
 <div class="container h-full mx-auto flex justify-center items-center">
 	<div class="space-y-10 text-center flex flex-col items-center">
-		<h2 class="h2">Welcome to Skeleton.</h2>
+		<h2 class="h2">Wilkommen bei AnoAsked.</h2>
 		<!-- Animated Logo -->
 		<figure>
 			<section class="img-bg" />
@@ -18,20 +20,15 @@
 			</svg>
 		</figure>
 		<!-- / -->
-		<div class="flex justify-center space-x-2">
-			<a
-				class="btn variant-filled"
-				href="https://skeleton.dev/"
-				target="_blank"
-				rel="noreferrer"
-			>
-				Launch Documentation
-			</a>
-		</div>
-		<div class="space-y-2">
-			<p>Try editing the following:</p>
-			<p><code class="code">/src/routes/+layout.svelte</code></p>
-			<p><code class="code">/src/routes/+page.svelte</code></p>
+		<div class="flex flex-col justify-center space-y-2">
+			<button type="button" class="btn variant-ghost-success">
+				<Icon icon="mdi:github" width="30"/>
+				<span>Mit Github Fortfahren</span>
+			</button>
+			<button type="button" class="btn variant-ghost-error">
+				<Icon icon="mdi:anonymous" width="30"/>
+				<span>Anonym Fortfahren</span>
+			</button>
 		</div>
 	</div>
 </div>
