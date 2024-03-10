988e8aa363bb39931c8ba89dd86493f431175f0f
Simon Masooglu <masooglu.simon@gmail.com>
2024-03-10 11:54:09 +0100

    Exec prettier

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -8,16 +8,16 @@
 		<!-- Animated Logo -->
 		<figure>
 			<section class="img-bg" />
-			<img src="/favicon.svg" alt="AnoAsked Logo" class="w-64 h-64 md:w-80 md:h-80"/>
+			<img src="/favicon.svg" alt="AnoAsked Logo" class="w-64 h-64 md:w-80 md:h-80" />
 		</figure>
 		<!-- / -->
 		<div class="flex flex-col justify-center space-y-2">
-			<button type="button" class="btn variant-ghost-success ">
-				<Icon icon="mdi:github" class="w-6 h-6"/>
+			<button type="button" class="btn variant-ghost-success">
+				<Icon icon="mdi:github" class="w-6 h-6" />
 				<span>Mit Github Fortfahren</span>
 			</button>
 			<button type="button" class="btn variant-ghost-error">
-				<Icon icon="mdi:anonymous" class="w-6 h-6"/>
+				<Icon icon="mdi:anonymous" class="w-6 h-6" />
 				<span>Anonym Fortfahren</span>
 			</button>
 		</div>
@@ -34,7 +34,8 @@
 	}
 	.img-bg {
 		@apply absolute z-[-1] rounded-full blur-[50px] transition-all;
-		animation: pulse 5s cubic-bezier(0, 0, 0, 0.5) infinite,
+		animation:
+			pulse 5s cubic-bezier(0, 0, 0, 0.5) infinite,
 			glow 5s linear infinite;
 	}
 	@keyframes glow {
