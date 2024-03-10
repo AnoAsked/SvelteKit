cba0d371ac99c08e4725915a5c81b4ef98fe7df2
Simon Masooglu <masooglu.simon@gmail.com>
2024-03-10 21:05:26 +0100

    Added test for welcome text for testing

---
+++ tests/test.ts
@@ -0,0 +1,6 @@
+import { expect, test } from '@playwright/test';
+
+test('index page has expected h2 welcome text', async ({ page }) => {
+	await page.goto('/');
+	await expect(page.getByRole('heading', { name: 'Willkommen bei AnoAsked' })).toBeVisible();
+});
