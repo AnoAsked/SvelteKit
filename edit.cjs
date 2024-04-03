e191a9c07afef1fc959ec28774e90f3700c39f4b
Simon Masooglu <masooglu.simon@gmail.com>
2024-04-03 13:32:43 +0200

    Added suggested libraries

--- edit.cjs
+++ edit.cjs
@@ -1,20 +1,20 @@
-const fs = require('fs');
+// const fs = require('fs');
 
-const filePath = 'node_modules/gun/sea.js'; // Replace with your actual file path
-const searchString = './lib/text-encoding'; // String to be replaced
-const replaceString = './lib/text-encoding/index.js'; // Replacement string
+// const filePath = 'node_modules/gun/sea.js'; // Replace with your actual file path
+// const searchString = './lib/text-encoding'; // String to be replaced
+// const replaceString = './lib/text-encoding/index.js'; // Replacement string
 
-fs.readFile(filePath, 'utf8', (err, data) => {
-  if (err) {
-    console.error(err);
-  } else {
-    const modifiedContent = data.replace(searchString, replaceString);
-    fs.writeFile(filePath, modifiedContent, 'utf8', (err) => {
-      if (err) {
-        console.error(err);
-      } else {
-        console.log('String replaced successfully!');
-      }
-    });
-  }
-});
+// fs.readFile(filePath, 'utf8', (err, data) => {
+//   if (err) {
+//     console.error(err);
+//   } else {
+//     const modifiedContent = data.replace(searchString, replaceString);
+//     fs.writeFile(filePath, modifiedContent, 'utf8', (err) => {
+//       if (err) {
+//         console.error(err);
+//       } else {
+//         console.log('String replaced successfully!');
+//       }
+//     });
+//   }
+// });
