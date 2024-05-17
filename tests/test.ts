1dc281f85c92b558f1673a82f47b8236090f9fb0
Simon Masooglu <masooglu.simon@gmail.com>
2024-05-18 00:34:05 +0200

    Changed heading test to english

--- tests/test.ts
+++ tests/test.ts
@@ -2,5 +2,5 @@ import { expect, test } from '@playwright/test';
 
 test('index page has expected h2 welcome text', async ({ page }) => {
 	await page.goto('/');
-	await expect(page.getByRole('heading', { name: 'Willkommen bei AnoAsked' })).toBeVisible();
+	await expect(page.getByRole('heading', { name: 'Welcome to AnoAsked' })).toBeVisible();
 });
